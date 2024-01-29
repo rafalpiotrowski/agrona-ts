2f48f051daa56e94ed50b4e368ebbe710717f8bf
rafalpiotrowski <rafal.piotorwski@live.com>
2024-01-29 17:51:56 +0100

    init code checking

---
+++ src/MutableDirectBuffer.ts
@@ -0,0 +1,24 @@
+import { ByteOrder } from "./ByteOrder";
+import { DirectBuffer } from "./DirectBuffer";
+
+export interface MutableDirectBuffer extends DirectBuffer {
+    putLong(index: number, value: bigint, byteOrder?: ByteOrder): void;
+    putInt(index: number, value: number, byteOrder?: ByteOrder): void;
+    putShort(index: number, value: number, byteOrder?: ByteOrder): void;
+    putFloat(index: number, value: number, byteOrder?: ByteOrder): void;
+    putDouble(index: number, value: number, byteOrder?: ByteOrder): void;
+    putByte(index: number, value: number): void;
+    putBytes(
+        index: number,
+        src: number[] | Uint8Array | DirectBuffer,
+        length: number,
+    ): void;
+    putBytesWithOffset(
+        index: number,
+        src: number[] | Uint8Array | DirectBuffer,
+        offset: number,
+        length: number,
+        byteOrder?: ByteOrder,
+    ): void;
+    putStringWithoutLengthAscii(index: number, value: string): void;
+}
---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import { BitUtil } from "./BitUtil";
+import { ByteOrder } from "./ByteOrder";
+import { StringBuilder } from "./StringBuilder";
+import { DirectBuffer } from "./DirectBuffer";
+import { MutableDirectBuffer } from "./MutableDirectBuffer";
+import { Iterator } from "./Iterator";
+import { DefaultMutableDirectBuffer } from "./DefaultMutableDirectBuffer";
+import { ExpandableDirectByteBuffer } from "./ExpandableDirectByteBuffer";
+
+export {
+    BitUtil,
+    ByteOrder,
+    StringBuilder,
+    DirectBuffer,
+    MutableDirectBuffer,
+    Iterator,
+    DefaultMutableDirectBuffer,
+    ExpandableDirectByteBuffer,
+};
